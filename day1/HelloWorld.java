6bafdc7c97ddd16724c9d6dcd19c058e29b4a9a3
SowmyaMahesh28 <142719616+SowmyaMahesh28@users.noreply.github.com>
2023-08-20 11:29:17 +0100

    Add files via upload

---
+++ day1/HelloWorld.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		//write a print statement
+		//tysys-control+space+enter
+		System.out.println("Lord Venkatesh");
+	}
+
+}
---
+++ day1/LearnIfCondtions.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class LearnIfCondtions {
+	public static void main(String[] args) {
+		//find out the number is postive or negative
+		
+		int number = 35;
+		//type if-->cntrl+space-->enter
+		
+		if(number>0)
+		{
+			System.out.println("The number is poisitive" + number + " is postive");
+			
+		}
+			else 
+			{
+				System.out.println("The nummber is negativ"+number+  "is negative");
+		}
+		
+	}
+
+}
